84995e75079257beb50ddc8ce81ba7c41db06ba9
unknown <hlong.24082k@gmail.com>
2022-04-29 16:13:11 +0700

    Lab07_1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -12,8 +12,17 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import Contact from './ContactComponent';
-import { Switch, Route, Redirect } from 'react-router-dom';
- 
+import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+const mapStateToProps = state => {
+  return {
+    dishes: state.dishes,
+    comments: state.comments,
+    promotions: state.promotions,
+    leaders: state.leaders
+  }
+}
 
 class Main extends Component {
 
@@ -47,19 +56,32 @@ class Main extends Component {
       );
     };
     return (
+      // <div>
+      //   <Header />
+      //   <Switch>
+      //       <Route path='/home' component={HomePage} />
+      //       <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+      //       <Route exact path='/contactus' component={Contact} />
+      //       <Route path='/menu/:dishId' component={DishWithId} />
+      //       <Redirect to="/home" />
+      //   </Switch>
+      //   <Footer />
+      // </div>
       <div>
         <Header />
-        <Switch>
-            <Route path='/home' component={HomePage} />
-            <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
-            <Route exact path='/contactus' component={Contact} />
-            <Route path='/menu/:dishId' component={DishWithId} />
-            <Redirect to="/home" />
-        </Switch>
+        <div>
+          <Switch>
+              <Route path='/home' component={HomePage} />
+              <Route exact path='/menu' component={() => <Menu dishes={this.props.dishes} />} />
+              <Route path='/menu/:dishId' component={DishWithId} />
+              <Route exact path='/contactus' component={Contact} /> 
+              <Redirect to="/home" />
+          </Switch>
+        </div>
         <Footer />
       </div>
     );
   }
 }
 
-export default Main;
+export default withRouter(connect(mapStateToProps)(Main));
