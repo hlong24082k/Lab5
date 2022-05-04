d4d41b8f1c5a977bafbf7a2c35dd044b95eee65e
unknown <hlong.24082k@gmail.com>
2022-05-04 21:27:52 +0700

    Lab08_2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -15,6 +15,8 @@ import Contact from './ContactComponent';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 
+import { addComment } from '../redux/ActionCreators';
+
 const mapStateToProps = state => {
   return {
     dishes: state.dishes,
@@ -23,6 +25,13 @@ const mapStateToProps = state => {
     leaders: state.leaders
   }
 }
+
+const mapDispatchToProps = dispatch => ({
+  
+  addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment))
+
+});
+
 
 class Main extends Component {
 
@@ -51,8 +60,10 @@ class Main extends Component {
     };
     const DishWithId = ({match}) => {
       return(
-          <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
-            comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+        <DishDetail dish={this.props.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
+        comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
+        addComment={this.props.addComment}
+        />
       );
     };
     return (
@@ -84,4 +95,4 @@ class Main extends Component {
   }
 }
 
-export default withRouter(connect(mapStateToProps)(Main));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Main));
