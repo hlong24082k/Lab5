b887ecfe0675bb14d3fe3a98ed150b7b8bbbcff8
hlong24082k <hlong.24082k@gmail.com>
2022-05-05 21:29:41 +0700

    Lab09_1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -15,7 +15,7 @@ import Contact from './ContactComponent';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 
-import { addComment } from '../redux/ActionCreators';
+import { addComment, fetchDishes } from '../redux/ActionCreators';
 
 const mapStateToProps = state => {
   return {
@@ -28,12 +28,15 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => ({
   
-  addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment))
-
+  addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment)),
+  fetchDishes: () => { dispatch(fetchDishes())}
 });
 
 
 class Main extends Component {
+  componentDidMount() {
+    this.props.fetchDishes();
+  }
 
   constructor(props) {
     super(props);
@@ -53,6 +56,8 @@ class Main extends Component {
       return(
         <Home 
             dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+            dishesLoading={this.props.dishes.isLoading}
+            dishesErrMess={this.props.dishes.errMess}
             promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
             leader={this.state.leaders.filter((leader) => leader.featured)[0]}/>
         
@@ -61,8 +66,10 @@ class Main extends Component {
     const DishWithId = ({match}) => {
       return(
         <DishDetail dish={this.props.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
-        comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
-        addComment={this.props.addComment}
+          isLoading={this.props.dishes.isLoading}
+          errMess={this.props.dishes.errMess}
+          comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
+          addComment={this.props.addComment}
         />
       );
     };
